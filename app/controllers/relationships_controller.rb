e3d68da1b8c8e2aaa749429f88a27dea68a4ad20
tsuruta <naritakundono@gmail.com>
2022-10-01 09:54:03 +0000

    [Add]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -12,7 +12,7 @@ class RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     user =User.find(params[:user_id])
-    @users = user.followongs
+    @users = user.followings
   end
   # フォロワ一覧
   def followers
