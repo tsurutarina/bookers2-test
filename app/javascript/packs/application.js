f58c8727c8dcf91e6fd240250a292f6d1c4d83ce
tsuruta <naritakundono@gmail.com>
2022-10-05 03:11:33 +0000

    [add]test7dtemplate

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,7 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 // 追加
-window.$ = window.jQuery = require('jquery');
+// window.$ = window.jQuery = require('jquery');
 
 Rails.start()
 Turbolinks.start()
